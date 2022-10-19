0309457f07ae3f154a13c7c90770c20be971772e
Geticom <geticom.web@gmail.com>
2022-10-19 15:37:28 -0300

    update-partnertships

--- src/components/Partnerships/index.js
+++ src/components/Partnerships/index.js
@@ -2,23 +2,22 @@ import cnpq from "../../assets/img/cnpq.jpg";
 import fapema from "../../assets/img/fapema.jpg";
 import pecs from "../../assets/img/pecs.jpg";
 import engcomp from "../../assets/img/engcomp.png";
-import usp from "../../assets/img/usp.jpg";
-import ifam from "../../assets/img/ifam.jpg";
 import fapead from "../../assets/img/fapead.jpg";
 import abtelecom from "../../assets/img/abtelecom.jpg";
 import fiema from "../../assets/img/fiema.jpg";
-import celplan from "../../assets/img/celplan.jpg";
 import lig16 from "../../assets/img/lig16.jpg";
 import cisco from "../../assets/img/cisco.jpg";
 import anatel from "../../assets/img/anatel.jpg";
 import tvn from "../../assets/img/tvn.jpg";
-import cla from "../../assets/img/cla.jpg";
+import uff from "../../assets/img/uff.jpg";
+import puc from "../../assets/img/pucrio.png";
 import crea from "../../assets/img/crea.jpg";
+import acm from "../../assets/img/academiamaranhense.png";
 import { Container} from "react-bootstrap";
 import './partnerships.css';
 
 export default function Partnerships() {
-    const partnershipsLogos = [cnpq, fapema, pecs, engcomp, usp, ifam, fapead, abtelecom, fiema, celplan, lig16, cisco, anatel, tvn, cla, crea];
+    const partnershipsLogos = [cnpq, fapema, pecs, engcomp, fapead, abtelecom, fiema, lig16, cisco, anatel, tvn, uff, puc, crea, acm];
 
     return (
         <Container className="partnerships">
